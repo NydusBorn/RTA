2b506371b2c9ab3f31af6cbda2b3e796b2208ea4
Nydus Born <90088743+NydusBorn@users.noreply.github.com>
2024-04-01 17:25:40 +0300

    Set up

--- Frontend/app.vue
+++ Frontend/app.vue
@@ -1,5 +1,20 @@
+<script>
+import mermaid from 'mermaid';
+mermaid.initialize({ startOnLoad: true });
+</script>
+
 <template>
   <div>
     <NuxtWelcome />
+    <pre class="mermaid">
+      graph LR
+      A --- B
+      B-->C[fa:fa-ban forbidden]
+      B-->D(fa:fa-spinner);
+    </pre>
   </div>
 </template>
+
+<style>
+
+</style>
